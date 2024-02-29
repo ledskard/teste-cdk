8a90489168f178304d3041248227062533c2dfc5
Luiz Cunha <49279873+ledskard@users.noreply.github.com>
2024-02-29 10:24:55 -0300

    aham

--- teste-cdk/lib/teste_cdk-stack.ts
+++ teste-cdk/lib/teste_cdk-stack.ts
@@ -4,33 +4,43 @@ import { Construct } from 'constructs';
 export class TesteCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const vpc = new ec2.Vpc(this, 'MinhaVPC', {
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        },
+      ],
+    });
+    const cluster = new ecs.Cluster(this, 'frontend-cluster', { vpc });
+    const repository = ecr.Repository.fromRepositoryName(this, 'frontend-repository', 'teste-cdk');
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
 
-    const vpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true });
-
-    const cluster = new ecs.Cluster(this, 'MyCluster', { vpc });
-
-    const repository = ecr.Repository.fromRepositoryName(this, 'MyRepository', 'teste-cdk');
-
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
-    const container = taskDefinition.addContainer('WebContainer', {
+    });
+    const container = taskDefinition.addContainer('frontend-next', {
       image: ecs.ContainerImage.fromEcrRepository(repository, 'v2.0'),
       portMappings: [{ containerPort: 3000 }],
     });
-
     const service = new ecs.FargateService(this, 'Service', {
       cluster,
       taskDefinition,
     });
-
     const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
       vpc,
       internetFacing: true,
     });
-
     const listener = alb.addListener('Listener', {
       port: 80,
     });
-
     listener.addTargets('ECS', {
       port: 80,
       targets: [service.loadBalancerTarget({
@@ -38,7 +48,6 @@ export class TesteCdkStack extends Stack {
         containerPort: 3000,
       })],
     });
-
     new CfnOutput(this, 'ALBDNS', { value: alb.loadBalancerDnsName });
   }
 }
